69571845eb8ca2cc53c3b5d09274daf4a643e4e8
Mandeep Condle <mandeepcondle@gmail.com>
2014-10-05 19:02:02 -0400

    naive python crawler complete

--- PythonWebCrawler/webcrawler.py
+++ PythonWebCrawler/webcrawler.py
@@ -6,15 +6,24 @@
 #
 
 from bs4 import BeautifulSoup
+import requests
 import urllib2
 
-seed = "http://www.google.com"
+seed = ""
+urls_to_crawl = 0
 
 
 # Returns the html contents of a page
 def get_html(url):
-	response = urllib2.urlopen(url)
-	html = response.read()
+	html = ""
+	if type(url) == str:		
+		pos1 = url.find("http://")
+		pos2 = url.find("https://")
+
+		if pos1 != -1 or pos2 != -1:
+			response = requests.get(url)
+			html = response.content
+
 	return html
 
 # Returns the next url to be crawled
@@ -44,25 +53,35 @@ def crawler_logic(seed):
 
 	to_crawl.append(seed)	#start with this url and proceed to conquer the world
 	next_link = seed
-	count = 1000;
 
-	while count > 0:
+	while len(crawled) < urls_to_crawl:
+		print "entering while"
+
+		if not to_crawl:	#in case the to_crawl is empty before the nubmer of urls to crawl
+			break
+		
 		next_link = to_crawl.pop()
-
 		if next_link not in crawled:
 			#crawl this link, add all of it's links to to_crawl
-			page_html = get_html(next_link)
+			page_html = get_html(next_link)						
+			page_links = get_all_links(page_html)			
+			graph[next_link] = page_links
 
-			if page_html:
-				page_links = get_all_links(page_html)			
-				to_crawl.append(page_links)	
+			for page in page_links:
+				to_crawl.append(page)					
 
 			crawled.append(next_link)				
 
-		count = count+1
-
-	return crawled
+	return graph
 
 if __name__ == "__main__":
+	seed = raw_input("Enter a seed URL (include http/s) : ")
+	urls_to_crawl = raw_input("Enter the number of URLs to crawl : ")
+
 	urls = crawler_logic(seed)
 	print urls
+	print len(urls)
+	# print graph
+
+
+
---
+++ README.md
@@ -0,0 +1,12 @@
+Crawling the web
+----------------
+
+1. Simple python web crawler
+   Takes a seed URL and number of URLs to crawl as inputs and starts crawling from seed. This isn't the most efficient implementation and might take a few seconds to spit out the result.
+
+
+2. 
+
+
+3. 
+ 
