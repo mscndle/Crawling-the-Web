4031abdd88a232f864226e50f4bb895acdead288
Mandeep Condle <mandeepcondle@gmail.com>
2014-10-12 16:40:56 -0400

    crawler complete

--- JavaWebCrawler/src/CrawlWeb.java
+++ JavaWebCrawler/src/CrawlWeb.java
@@ -8,15 +8,11 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
 import java.util.HashSet;
 import java.util.InputMismatchException;
 import java.util.Set;
-
-
+import java.util.Stack;
 
 
 // Crawling the web in java
@@ -27,7 +23,7 @@ public class CrawlWeb {
 
     public static void main(String[] args) throws IOException {
 
-        //Not handling incorrect inputs for now
+////        Not handling incorrect inputs for now
 //        System.out.println("Enter URL to begin crawling");
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        url = br.readLine();
@@ -35,21 +31,29 @@ public class CrawlWeb {
 //        System.out.println("Enter total number of URLs to crawl");
 //        crawlSize = Integer.parseInt(br.readLine());
 
-        String url = "http://www.google.com";
-
+//        String url = "http://www.google.com";
+//
 //        String page = getHtml(url);
 //        String page = getHtmlUsingJsoup(url);
-//
-        Set<String> pageLinks = getAllLinks(url);
-        for (String s : pageLinks) {
-            System.out.println(s);
-        }
 
+//        Set<String> pageLinks = getAllLinks(url);
+//        for (String s : pageLinks) {
+//            System.out.println(s);
+//        }
+
+        url = "https://www.google.com";
+        crawlSize = 1000;
+
+        Set<String> crawledLinks = crawlWeb(url, crawlSize);
+
+        for (String link : crawledLinks) {
+            System.out.println(link);
+        }
     }
 
 
     //
-    //  Standard java way of 
+    //  Using standard java libraries to read html contents of a page
     //
     static String getHtml(String url) {
         StringBuilder sb = new StringBuilder();
@@ -85,18 +89,38 @@ public class CrawlWeb {
         return html;
     }
 
-    static void crawlWeb(String origin, int limit) {
-
+    //
+    //  Crawls the web starting from origin url in a DFS manner using a stack
+    //
+    static Set<String> crawlWeb(String origin, int limit) {
         Set<String> crawled = new HashSet<String>();    //maintains a list of unique urls crawled
-        Set<String> toCrawl = new HashSet<String>();
+        Stack<String> toCrawl = new Stack<String>();
+        Set<String> links;
+        String url;
 
-        toCrawl.add(origin);
+        toCrawl.push(origin);
 
         while (!toCrawl.isEmpty() || crawled.size() < limit) {
+            url = toCrawl.pop();
 
+            if (!crawled.contains(url)) {   //crawl if not already crawled
 
+                // To crawl - grab the links in that page add it to the stack
+                links = getAllLinks(url);
+
+                // Should probably check for duplicates here as well
+                for (String link : links) {
+                    if (!crawled.contains(link)) {
+                        toCrawl.push(link);
+                    }
+                }
+
+                crawled.add(url);
+                System.out.println("crawled: " + url);
+            }
         }
 
+        return crawled;
     }
 
     //
@@ -111,21 +135,27 @@ public class CrawlWeb {
         String attribute;
 
         try {
-            doc = Jsoup.connect(url).get();
+            doc = Jsoup.connect(url).timeout(3*1000).get();
             elements = doc.select("a");
+        } catch (SocketTimeoutException ste) {
+            ste.printStackTrace();
         } catch (IOException ioe) {
             ioe.printStackTrace();
+            System.out.println("attempting to connect to: " + url);
         }
 
         for (Element a : elements) {
             attribute = a.attr("href");
 
             //handle cases with http and https missing
-            if (!attribute.contains("http") && !attribute.contains("https")) {
-                attribute = url + attribute;
+            //for now ignore the url that doesn't have http/https
+            if (attribute.length() >= 5 && (attribute.substring(0,4).contains("http") ||
+                    attribute.substring(0,5).contains("https"))) {
+
+                pageLinks.add(attribute);
+//                System.out.println("added url : " + attribute);
             }
 
-            pageLinks.add(attribute);
         }
 
         return pageLinks;
